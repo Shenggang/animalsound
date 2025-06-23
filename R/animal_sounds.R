bbccfaca6438bc2f02acc937844adc77d8f7dd94
Shenggang Hu <hushenggang1996@163.com>
2025-06-23 13:22:54 +0100

    create a documented function

--- R/animal_sounds.R
+++ R/animal_sounds.R
@@ -1,3 +1,15 @@
+#' animalsound package
+#'
+#' animalsound function, part of R Workshop example package
+#'
+#' @param animal A non-numerical string of length greater than 1
+#' @param sound A non-numerical string of length greater than 1
+#'
+#' @return A string composed using the inputs
+#' @export
+#'
+#' @examples
+#' animal_sounds('dog', 'woolf')
 animal_sounds <- function(animal, sound) {
   stopifnot(is.character(animal) & length(animal) == 1)
   stopifnot(is.character(sound) & length(sound) == 1)
